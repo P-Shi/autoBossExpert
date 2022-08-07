25798a945d449b1cc4f40c06b99d4df6e79dbdec
Pavel <p-shi@mail.ru>
2022-08-07 14:27:10 +0500

    Профиль

---
+++ dev/static/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    svg4everybody({});
+});
+
+
+// Полифилы
+
+// forEach IE 11
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+    console.info('polyfill for IE11');
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
+
+// closest IE 11
+(function () {
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+})();
+
+// matches IE 11
+(function () {
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+    }
+})();
+
+//Array.form IE 11
+if (!Array.from) {
+    Array.from = function (object) {
+        'use strict';
+        return [].slice.call(object);
+    };
+}
+
+$(document).ready(function(){
+	$(document).on('click', '.block__more', function(e){
+		e.preventDefault()
+		$(this).toggleClass('open')
+		$(this).siblings('.block__hidden').slideToggle()
+	})
+})
+$(document).ready(function(){
+	$(document).on('click', '.open__nav', function(e){
+		e.preventDefault()
+		$(this).toggleClass('open')
+		$('header .nav').slideToggle()
+	})
+})
